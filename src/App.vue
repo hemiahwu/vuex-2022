f367adb85b471dc6adebe320169ab465002e3f1d
hemiahwu <309595700@qq.com>
2022-03-14 21:28:13 +0800

    03

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<template></template>
+<template>
+  <ProductListOne :products="products" />
+  <ProductListTwo :products="products" />
+</template>
 
 <script setup>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
+import { ref } from "vue";
+
+const products = ref([
+  { name: "马云", money: 100 },
+  { name: "马化腾", money: 90 },
+  { name: "马冬梅", money: 20 },
+  { name: "吴海洋", money: 10 },
+]);
 </script>
 
 <style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
 </style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="product-list-one">
+    <h2>组件1</h2>
+    <ul>
+      <li v-for="product in products" :key="product.name">
+        <span class="name">{{ product.name }}</span>
+        <span class="money">¥{{ product.money }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  products: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
+}
+#product-list-one ul {
+  padding: 0;
+}
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
+}
+.money {
+  font-weight: bold;
+  color: #e8800c;
+}
+</style>
